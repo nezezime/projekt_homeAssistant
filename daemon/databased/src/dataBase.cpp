8aba8ca3e39c0e3b022f65d78eb7351a32845546
bob <bob@localhost.localdomain>
2018-10-15 22:03:26 +0200

    db: functional makefile

--- daemon/databased/src/dataBase.cpp
+++ daemon/databased/src/dataBase.cpp
@@ -1,9 +1,25 @@
 #include "dataBase.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-  printf("databased running");
+  std::cout << "databased started" << std::endl;
   
+  database::RunServer();
 
   return 0;
+}
+
+/************ FUNCTION DEFINITIONS ***************************************************************/
+void database::RunServer(void)
+{
+  std::cout << "RunServer" << std::endl;
+  std::string server_address("0.0.0.0:50051");
+  //RouteGuideImpl service(db_path);
+
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  //builder.RegisterService(&service);
+  //std::unique_ptr<Server> server(builder.BuildAndStart());
+  //std::cout << "Server listening on " << server_address << std::endl;
+  //server->Wait();
 }
--- daemon/databased/src/dataBase.h
+++ daemon/databased/src/dataBase.h
@@ -1,10 +1,25 @@
 #ifndef DATA_BASE_H
 #define DATA_BASE_H
 
+#include <algorithm>
+#include <chrono>
+#include <iostream>
 #include <stdio.h>
 #include <string>
+#include <unistd.h>
 
-#include <grpcpp/grpcpp.h>
-#include "storage.grpc.pb.h"
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+
+//includi zbuildane kode -> TODO popravi pot
+#include "../storage.grpc.pb.h"
+
+namespace database
+{
+  void RunServer(void);
+}
 
 #endif
