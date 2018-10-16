9a3271cc5509bc1a65c5c67ea06539f8d8ad7b1b
bob <bob@localhost.localdomain>
2018-10-16 22:08:31 +0200

    ws: added client hello RPC

--- daemon/webserviced/src/webService.cpp
+++ daemon/webserviced/src/webService.cpp
@@ -1,7 +1,71 @@
 #include "webService.h"
 
+/************************************* FUNCTION PROTOTYPES ***************************************/
+
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+using databaseRPC::dbRPC;
+
+/************************************** CLASS DEFINITIONS *****************************************/
+class ClientDbRPC
+{
+public:
+  ClientDbRPC(std::shared_ptr<Channel> channel) : stub_(dbRPC::NewStub(channel))
+  {
+    //TODO add db parse if required
+    //std::cout << "client constructor" << std::endl;
+  }
+
+  void RPC_Hello(void)
+  {
+    databaseRPC::HelloRequest request;
+    databaseRPC::HelloReply reply;
+
+    //populate request
+    request.set_name("test");
+    request.set_message_id(1234);
+
+    //send RPC
+    int result = RPC_Hello_Execute(request, &reply);
+    if(result != 0)
+    {
+      std::cout << "RPC failed with error code " << result << std::endl;
+      return;
+    }
+
+    //parse response
+    std::cout << "RPC response OK for message_id " << reply.message_id() << std::endl;
+  }
+
+private:
+  std::unique_ptr<dbRPC::Stub> stub_;
+
+  int RPC_Hello_Execute(const databaseRPC::HelloRequest &request, databaseRPC::HelloReply *reply)
+  {
+    ClientContext context;
+    Status status = stub_->Hello(&context, request, reply);
+    return status.error_code();
+  }
+};
+
 int main()
 {
-  printf("Hello");
+  std::cout << "webserviced started" << std::endl;
+
+  //create a gRPC stub and connect to gRPC channel without SSL
+  //CreateCustomChannel adds more configuration options
+  ClientDbRPC rpc_client_db(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+
+  //call Hello RPC
+  rpc_client_db.RPC_Hello();
+
+
   return 0;
 }
+
+/************************************ FUNCTION DEFINITIONS ***************************************/
--- daemon/webserviced/src/webService.h
+++ daemon/webserviced/src/webService.h
@@ -1,6 +1,23 @@
 #ifndef WEB_SERVICE_H
 #define WEB_SERVICE_H
 
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <random>
 #include <stdio.h>
+#include <string>
+#include <thread>
+#include <unistd.h>
+
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+
+//includi zbuildane kode -> TODO popravi pot
+#include "../storage.grpc.pb.h"
 
 #endif
