c9e415dad4021b23ffc4a7a296450119b3636cb1
bob <bob@localhost.localdomain>
2018-10-26 14:27:57 +0200

    db: sql connector all code

--- daemon/databased/src/dataBase.cpp
+++ daemon/databased/src/dataBase.cpp
@@ -11,6 +11,11 @@ using grpc::ServerWriter;
 using grpc::Status;
 using databaseRPC::dbRPC;
 
+//using namespace std;
+using namespace ::sql;
+
+
+
 /************************************** CLASS DEFINITIONS *****************************************/
 
 /**
@@ -43,6 +48,62 @@ public:
 int main(int argc, char **argv)
 {
   std::cout << "databased started" << std::endl;
+
+  //connect to database
+
+/* x DevAPI
+  try
+  {
+    mysqlx::Session mySession(mysqlx::SessionOption::HOST, "192.168.1.2",
+                              //mysqlx::SessionOption::PORT, 3306,
+                              mysqlx::SessionOption::USER, "username",
+                              mysqlx::SessionOption::PWD, "password",
+                              mysqlx::SessionOption::DB, "home_assistant"
+                              );
+  }
+  catch(const ::mysqlx::Error &mysql_error)
+  {
+    std::cout << "mysql error caught: " << mysql_error << std::endl;
+    exit(1);
+  }
+*/
+
+  //JDBC API
+  try
+  {
+    sql::Driver * driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+
+    /* Create a connection */
+    driver = sql::mysql::get_mysql_driver_instance();
+    con = driver->connect("tcp://192.168.1.2:3306", "username", "password");
+    /* Connect to the MySQL test database */
+    con->setSchema("home_assistant");
+
+    stmt = con->createStatement();
+    res = stmt->executeQuery("SELECT * FROM users");
+    while(res->next())
+    {
+      std::cout << "\t... MySQL replies: ";
+      /* Access column data by alias or column name */
+      std::cout << res->getString("username") << std::endl;
+      std::cout << "\t... MySQL says it again: ";
+      /* Access column data by numeric offset, 1 is the first column */
+      std::cout << res->getString(1) << std::endl;
+    }
+  }
+  catch(sql::SQLException &e)
+  {
+    std::cout << "# ERR: SQLException in " << __FILE__;
+    std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+    std::cout << "# ERR: " << e.what();
+    std::cout << " (MySQL error code: " << e.getErrorCode();
+    std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+  }
+
+
 
   database::RunServer();
 
--- daemon/databased/src/dataBase.h
+++ daemon/databased/src/dataBase.h
@@ -4,10 +4,14 @@
 #include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <unistd.h>
+#include <vector>
 
+//grpc
 #include <grpc/grpc.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
@@ -15,6 +19,31 @@
 #include <grpcpp/security/server_credentials.h>
 
 #include "storage.grpc.pb.h"
+
+//mysql connector
+//#include <mysqlx/xdevapi.h> //x DevAPI
+
+//JDBC
+#include <jdbc/mysql_driver.h>
+#include <jdbc/mysql_connection.h>
+
+//TODO optimize includes
+/*#include <jdbc/cppconn/build_config.h>
+#include <jdbc/cppconn/config.h>
+#include <jdbc/cppconn/connection.h>
+#include <jdbc/cppconn/datatype.h>
+#include <jdbc/cppconn/metadata.h>
+#include <jdbc/cppconn/parameter_metadata.h>
+#include <jdbc/cppconn/prepared_statement.h>
+#include <jdbc/cppconn/resultset_metadata.h>
+#include <jdbc/cppconn/sqlstring.h>
+#include <jdbc/cppconn/variant.h>
+#include <jdbc/cppconn/version_info.h>
+#include <jdbc/cppconn/warning.h>*/
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
 
 namespace database
 {
