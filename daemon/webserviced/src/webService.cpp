3d20d66718892054e84e39158b35373fc03ee192
bob <bob@localhost.localdomain>
2018-11-05 20:48:05 +0100

    ws: basic gsoap implementation

--- daemon/webserviced/src/webService.cpp
+++ daemon/webserviced/src/webService.cpp
@@ -1,4 +1,5 @@
 #include "webService.h"
+#include "HASOAP.nsmap"
 
 /************************************* FUNCTION PROTOTYPES ***************************************/
 
@@ -53,7 +54,21 @@ private:
   }
 };
 
-int main()
+void *SoapProcessRequest(void * soap)
+{
+  pthread_detach(pthread_self());
+  //struct soap * soap_struct = static_cast<struct soap *> (soap);
+  HASOAPService ha_service(static_cast<struct soap *> (soap));
+  ha_service.serve();
+  /*soap_destroy(soap_struct);
+  soap_end(soap_struct);
+  soap_done(soap_struct);*/
+  ha_service.destroy();
+  free(soap);
+  return nullptr;
+}
+
+int main(int argc, char **argv)
 {
   std::cout << "webserviced started" << std::endl;
 
@@ -64,7 +79,66 @@ int main()
   //call Hello RPC
   rpc_client_db.RPC_Hello();
 
+  //SOAP multithreaded server initialization
+  struct soap soap;
+  struct soap * soap_safe_copy;
+  SOAP_SOCKET soap_socket, s;
+  soap_init(&soap);
+  //soap_imode(&soap, SOAP_XML_STRICT);
+  soap.send_timeout = soap_send_timeout;
+  soap.recv_timeout = soap_recv_timeout;
+  //soap.accept_timeout = soap_accept_timeout; //stops SOAP server after set time of inactivity
+  //soap.max_keep_alive = soap_max_keep_alive;
+  pthread_t tid;
 
+
+  //reuse address
+  soap.bind_flags |= SO_REUSEADDR;
+
+  //bind the socket
+  soap_socket = soap_bind(&soap, nullptr, soap_server_port, soap_max_req_backlog);
+  if(!soap_valid_socket(soap_socket))
+  {
+    std::cerr << "webserviced error invalid SOAP server socket " << soap_socket << std::endl;
+    exit(1);
+  }
+
+  std::cout << "SOAP server running @"<< "TODO address:"<< soap_server_port << std::endl;
+
+  //request loop
+  for(;;)
+  {
+    s = soap_accept(&soap);
+    if(!soap_valid_socket(s))
+    {
+      if(soap.errnum)
+      {
+        soap_print_fault(&soap, stderr);
+        exit(1);
+      }
+      std::cout << "SOAP server timed out" << std::endl;
+      break;
+    }
+
+    soap_safe_copy = soap_copy(&soap);
+    if(!soap_safe_copy)
+    {
+      std::cout << "SOAP server soap struct safe copy failed" << std::endl;
+      break;
+    }
+
+    pthread_create(&tid, nullptr, (void *(*)(void *))SoapProcessRequest, (void *)soap_safe_copy);
+  }
+
+  //detach soap struct and close socket
+  soap_closesocket(soap_socket);
+  soap_done(&soap);
+  return 0;
+}
+
+int HASOAPService::GetDateTime(const std::string& ha__GetDateTimeRequest, std::string &ha__GetDateTimeResponse)
+{
+  std::cout << "GetDateTime" << std::endl;
   return 0;
 }
 
--- daemon/webserviced/src/webService.h
+++ daemon/webserviced/src/webService.h
@@ -5,18 +5,30 @@
 #include <chrono>
 #include <iostream>
 #include <memory>
+#include <pthread.h>
 #include <random>
 #include <stdio.h>
 #include <string>
-#include <thread>
 #include <unistd.h>
 
+//includes for gRPC
+#include "storage.grpc.pb.h"
 #include <grpc/grpc.h>
 #include <grpcpp/channel.h>
 #include <grpcpp/client_context.h>
 #include <grpcpp/create_channel.h>
 #include <grpcpp/security/credentials.h>
 
-#include "storage.grpc.pb.h"
+//includes for gSOAP
+#include "soapHASOAPService.h"
+#include "soapH.h"
+
+//constants
+constexpr unsigned int soap_server_port = 26000;
+constexpr unsigned int soap_send_timeout = 10; //seconds
+constexpr unsigned int soap_recv_timeout = 10; //seconds
+constexpr unsigned int soap_accept_timeout = 0; //seconds
+constexpr unsigned int soap_max_keep_alive = 100;
+constexpr unsigned int soap_max_req_backlog = 10;
 
 #endif
